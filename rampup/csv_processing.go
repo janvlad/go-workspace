7ea41a5acea0673f1922b03695d8a4d2daaa1ca3
Jan Paulo <paulojan54@gmail.com>
2023-10-05 13:29:26 -0300

    tests

--- rampup/csv_processing.go
+++ rampup/csv_processing.go
@@ -120,6 +120,7 @@ func main() {
 		csvMap[id] = NewTransaction(record[0], record[1], record[2], record[3], record[4], record[5], record[6], record[7], record[8], record[9])
 	}
 
-	fmt.Println(csvMap[0])
+	fmt.Println("Average: ", TransactionAverage("Milk powder, butter, and cheese", "All", "Friday", csvMap))
+	// fmt.Println("Largest exporter: ", TransactionAverage("Logs, wood, and wood articles", csvMap))
 
 }
